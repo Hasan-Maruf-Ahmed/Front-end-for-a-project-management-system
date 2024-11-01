67d80d744d2480a1ed30107fdca574798b3e6b90
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-11-01 16:37:22 +0600

    Created folder for styles. Added index css.

--- index.php
+++ index.php
@@ -3,16 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <link rel="stylesheet" href="./styles/index.css"/>
+    <title>Project Management System</title>
 </head>
 <body>
-    <div>
-        <div>
+    <div class="home-page">
+        <div class="navbar">
         <?php
             include './components/nav.php'
         ?>
         </div>
-        <div><p>main page</p></div>
+        <div class="main-page"><p>main page</p></div>
     </div>
 </body>
 </html>
