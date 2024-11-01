d66e60479676215b50cc7f33ef0d81cd4b8de465
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-11-01 20:32:14 +0600

    created a completed project pregress section

--- index.php
+++ index.php
@@ -15,7 +15,51 @@
             include './components/nav.php'
         ?>
         </div>
-        <div class="main-page"><p>main page</p></div>
+        <div class="main-page">
+            <div class="main-left">
+                hello
+            </div>
+            <div class="main-right">
+                <div class="progress-content-wrapper">
+                    <span>Projects Complete</span>
+                    <div class="progress-section">
+                        progress
+                    </div>
+                    <div class="completed-list">
+                        <ul>
+                            <li>
+                                <i class="ph ph-rows"></i>
+                                <div class="complete-progress-detail">
+                                    <span class="title">Marketing Campaign</span>
+                                    <span class="task-no">2 tasks</span>
+                                </div>
+                            </li>
+                            <li>
+                                <i class="ph ph-rows"></i>
+                                <div class="complete-progress-detail">
+                                    <span class="title">Marketing Campaign</span>
+                                    <span class="task-no">2 tasks</span>
+                                </div>
+                            </li>
+                            <li>
+                                <i class="ph ph-rows"></i>
+                                <div class="complete-progress-detail">
+                                    <span class="title">Marketing Campaign</span>
+                                    <span class="task-no">2 tasks</span>
+                                </div>
+                            </li>
+                            <li>
+                                <i class="ph ph-rows"></i>
+                                <div class="complete-progress-detail">
+                                    <span class="title">Marketing Campaign</span>
+                                    <span class="task-no">2 tasks</span>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
