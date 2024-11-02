a792d0cbde2dfd38fcd8c3274a13757e4cdb19e4
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-11-02 13:51:53 +0600

    Created a new page for signing in.

--- components/nav.php
+++ components/nav.php
@@ -4,4 +4,4 @@
     <li><i class="ph ph-rows"></i></li>
     <li><i class="ph ph-sign-out"></i></li>
 </ul>
-<span class="user-icon"><i class="ph ph-user-circle"></i></span>
+<span class="user-icon"><a href="./pages/login.php"><i class="ph ph-user-circle"></i></a></span>
