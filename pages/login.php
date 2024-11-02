dd08f30b51ea72a0d90006993d545b57bd277022
Hasan Maruf Ahmed <hasanmarufahmedfahim@gmail.com>
2024-11-02 15:16:34 +0600

    Created a Login page.

--- pages/login.php
+++ pages/login.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../styles/variables.css" />
+    <link rel="stylesheet" href="../styles/login_signup.css" />
+    <title>Project Management System</title>
 </head>
+
 <body>
-    
+    <div class="sign_in_up-container">
+        <div class="heading">
+            <span class="primary-heading">Welcome Back!</span>
+            <span class="secondary-heading">Please, sign in to continue</span>
+        </div>
+        <form class="input-form" action="">
+            <div>
+                <label for="email">Email address</label>
+                <input class="input-box" type="email" id="email" name="email">
+            </div>
+            <div>
+                <label for="password">Password</label>
+                <input class="input-box" type="password" id="password" name="password">
+            </div>
+            <input class="sub-btn" type="submit" value="Submit">
+        </form>
+        <span class="alternate-link">Don't have an account? <a href="./signup.php">Sign up</a></span>
+    </div>
 </body>
+
 </html>
